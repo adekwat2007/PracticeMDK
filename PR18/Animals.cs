b3d57e1b220bf811bbfdbb900b0f6e0da8ed403e
Vazik <nagimovvazir@gmail.com>
2025-10-07 11:14:06 +0500

    PR17

---
+++ PR18/Animals.cs
@@ -0,0 +1,50 @@
+﻿namespace PR18
+{
+    internal abstract class Animal
+    {
+        public abstract void MakeSound();
+
+        public abstract void Eat();
+    }
+
+    internal interface IMovable
+    {
+        void Move();
+    }
+
+    internal class Cat : Animal, IMovable
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("МЯУ");
+        }
+
+        public override void Eat()
+        {
+            Console.WriteLine("Кот ест");
+        }
+
+        public void Move()
+        {
+            Console.WriteLine("Кот идет");
+        }
+    }
+
+    internal class Dog : Animal, IMovable
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("ГАВ");
+        }
+
+        public override void Eat()
+        {
+            Console.WriteLine("Собака ест");
+        }
+
+        public void Move()
+        {
+            Console.WriteLine("Собака идет");
+        }
+    }
+}
---
+++ PR18/Program.cs
@@ -0,0 +1,151 @@
+﻿namespace PR18
+{
+    //1
+    internal abstract class Figure
+    {
+        public abstract double GetArea();
+    }
+
+    internal class Circle : Figure
+    {
+        public double radius;
+
+        public override double GetArea()
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+
+    internal class Rectangle : Figure
+    {
+        public double a;
+        public double b;
+
+        public override double GetArea()
+        {
+            return a * b;
+        }
+    }
+
+    //2
+    internal abstract class Transport
+    {
+        public abstract void Start();
+
+        public abstract void Stop();
+    }
+
+    internal class Car : Transport
+    {
+        public override void Start()
+        {
+            Console.WriteLine("Машина начала движение");
+        }
+
+        public override void Stop()
+        {
+            Console.WriteLine("Машина остановилась");
+        }
+    }
+
+    internal class Bicycle : Transport
+    {
+        public override void Start()
+        {
+            Console.WriteLine("Велосипед начал движение");
+        }
+
+        public override void Stop()
+        {
+            Console.WriteLine("Велосипед остановился");
+        }
+    }
+
+    //3
+    internal interface IPlayable
+    {
+        void Play();
+
+        void Pause();
+    }
+
+    internal class MusicPlayer : IPlayable
+    {
+        public void Pause()
+        {
+            Console.WriteLine("Музыкальный плеер на паузе");
+        }
+
+        public void Play()
+        {
+            Console.WriteLine("Музыкальный плеер играет");
+        }
+    }
+
+    internal class VideoPlayer : IPlayable
+    {
+        public void Pause()
+        {
+            Console.WriteLine("Видео плеер на паузе");
+        }
+
+        public void Play()
+        {
+            Console.WriteLine("Видео плеер играет");
+        }
+    }
+
+    //4
+    internal abstract class Device
+    {
+        public abstract void TurnOn();
+    }
+
+    internal interface IRechargeable
+    {
+        void Recharge();
+    }
+
+    internal class Smartphone : Device, IRechargeable
+    {
+        public override void TurnOn()
+        {
+            Console.WriteLine("Смартфон включился");
+        }
+
+        public void Recharge()
+        {
+            Console.WriteLine("Смартфон зарядился");
+        }
+    }
+
+    //5
+    internal interface IPayment
+    {
+        void Pay(decimal amount);
+    }
+
+    internal class CreditCardPayment : IPayment
+    {
+        public void Pay(decimal amount)
+        {
+            Console.WriteLine($"Оплата по карте: {amount}руб");
+        }
+    }
+
+    internal class PayPalPayment : IPayment
+    {
+        public void Pay(decimal amount)
+        {
+            Console.WriteLine($"Оплата через PayPal: {amount}руб");
+        }
+    }
+
+ 
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
